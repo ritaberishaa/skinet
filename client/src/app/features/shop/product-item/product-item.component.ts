98b89ebf8ac54653d0b6645cdef02828dc52d333
Rita Berisha <rita.beris@outlook.com>
2025-09-30 15:57:35 +0200

    enhance product display and improve layout responsiveness

---
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { Product } from '../../../shared/models/product';
+import { MatCard, MatCardActions } from "@angular/material/card";
+import { MatCardContent } from "@angular/material/card";
+import { CurrencyPipe } from '@angular/common';
+import { MatIcon } from '@angular/material/icon';
+
+
+@Component({
+  selector: 'app-product-item',
+  standalone: true,
+  imports: [MatCard, MatCardContent, CurrencyPipe, MatCardActions, MatIcon],
+  templateUrl: './product-item.component.html',
+  styleUrl: './product-item.component.scss'
+})
+export class ProductItemComponent {
+  @Input() product?: Product;
+}
--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -2,11 +2,13 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ShopService } from '../../core/services/shop.service';
 import { Product } from '../../shared/models/product';
 import { MatCard } from '@angular/material/card';
+import { ProductItemComponent } from "./product-item/product-item.component";
+
 
 @Component({
   selector: 'app-shop',
   standalone: true,
-  imports: [MatCard],
+  imports: [ProductItemComponent],
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.scss']
 })
