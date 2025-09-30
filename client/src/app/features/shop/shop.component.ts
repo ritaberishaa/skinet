876d1da023900402061e2e34f4d95f39ef33d6c2
Rita Berisha <rita.beris@outlook.com>
2025-09-30 16:11:41 +0200

    implement brand and type fetching in shop component

--- client/src/app/features/shop/shop.component.ts
+++ client/src/app/features/shop/shop.component.ts
@@ -1,7 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ShopService } from '../../core/services/shop.service';
 import { Product } from '../../shared/models/product';
-import { MatCard } from '@angular/material/card';
 import { ProductItemComponent } from "./product-item/product-item.component";
 
 
@@ -17,6 +16,12 @@ export class ShopComponent implements OnInit {
   products: Product[] = [];
 
   ngOnInit(): void {
+    this.initializeShop();
+  }
+
+  initializeShop() {
+    this.shopService.getBrands();
+    this.shopService.getTypes();
     this.shopService.getProducts().subscribe({
       next: response => this.products = response.data,
       error: error => console.log(error)
