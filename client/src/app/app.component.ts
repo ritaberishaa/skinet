48ad0d6ac5d7024be0ab512f667ceac43d16cf5f
Rita Berisha <rita.beris@outlook.com>
2025-10-07 16:47:18 +0200

    refactor routing and header component for improved navigation

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,14 +1,12 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { RouterOutlet } from "@angular/router";
 import { HeaderComponent } from "./layout/header/header.component";
-import { Product } from './shared/models/product';
-import { ShopService } from './core/services/shop.service';
 import { ShopComponent } from "./features/shop/shop.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, ShopComponent],
+  imports: [RouterOutlet, HeaderComponent, ShopComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatIcon} from "@angular/material/icon";
 import { MatBadge} from "@angular/material/badge";
 import { CommonModule } from '@angular/common';
+import { Router, NavigationEnd, RouterLink, RouterLinkActive } from '@angular/router';
+import { filter, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-header',
@@ -9,13 +11,46 @@ import { CommonModule } from '@angular/common';
   imports: [
     MatIcon,
     MatBadge,
-    CommonModule
-  ],
+    CommonModule,
+    RouterLink,
+    RouterLinkActive
+],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  private router = inject(Router);
   mobileMenuOpen = false;
+  currentPage = 'HOME';
+
+  constructor() {
+    // Listen to route changes
+    this.router.events.pipe(
+      filter(event => event instanceof NavigationEnd),
+      map(event => event as NavigationEnd)
+    ).subscribe(event => {
+      this.updateCurrentPage(event.url);
+    });
+    
+    // Set initial page
+    this.updateCurrentPage(this.router.url);
+  }
+
+  private updateCurrentPage(url: string): void {
+    if (url === '/' || url === '') {
+      this.currentPage = 'HOME';
+    } else if (url.startsWith('/shop')) {
+      this.currentPage = 'SHOP';
+    } else if (url.startsWith('/contact')) {
+      this.currentPage = 'CONTACT';
+    } else {
+      this.currentPage = 'HOME';
+    }
+  }
+
+  getPageTitle(): string {
+    return this.currentPage;
+  }
 
   toggleMobileMenu(): void {
     this.mobileMenuOpen = !this.mobileMenuOpen;
