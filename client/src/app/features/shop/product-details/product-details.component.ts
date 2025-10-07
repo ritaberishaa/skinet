faafd5db882f4060754eed466b0f02cd1a2f16c4
Rita Berisha <rita.beris@outlook.com>
2025-10-07 17:06:23 +0200

    implement product details view with loading state and navigation

--- client/src/app/features/shop/product-details/product-details.component.ts
+++ client/src/app/features/shop/product-details/product-details.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { ShopService } from '../../../core/services/shop.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../../../shared/models/product';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [CommonModule, CurrencyPipe, MatButton, MatIcon],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss'
 })
-export class ProductDetailsComponent {
+export class ProductDetailsComponent implements OnInit {
+  private shopService = inject(ShopService);
+  private activatedRoute = inject(ActivatedRoute);
+  private router = inject(Router);
+  product?: Product;
 
+  ngOnInit(): void {
+    this.loadProduct();
+  }
+
+  loadProduct() {
+    const id = this.activatedRoute.snapshot.paramMap.get('id');
+    if (!id) return;
+    this.shopService.getProduct(+id).subscribe({
+      next: product => this.product = product,
+      error: error => console.log(error)
+    })
+  }
+
+  goBack() {
+    this.router.navigate(['/shop']);
+  }
 }
--- client/src/app/features/shop/product-item/product-item.component.ts
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -4,12 +4,13 @@ import { MatCard, MatCardActions } from "@angular/material/card";
 import { MatCardContent } from "@angular/material/card";
 import { CurrencyPipe } from '@angular/common';
 import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-product-item',
   standalone: true,
-  imports: [MatCard, MatCardContent, CurrencyPipe, MatCardActions, MatIcon],
+  imports: [MatCard, MatCardContent, CurrencyPipe, MatCardActions, MatIcon, RouterLink],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss'
 })
